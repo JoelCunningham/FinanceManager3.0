dd798d139b5f9dbed1dd54cc20615bd174984ee3
JoelCunningham <s3949308@student.rmit.edu.au>
2026-02-11 21:47:49 +1100

    FMUS4 - Categorise Transactions - Creates base code for AI categorisation

---
+++ FinanceManager.Application/Interfaces/IAiService.cs
@@ -0,0 +1,9 @@
+﻿using FinanceManager.Application.DTOs;
+
+namespace FinanceManager.Application.Interfaces
+{
+    public interface IAiService
+    {
+        Task<string> GenerateCategorySuggestionAsync(ReviewTransaction transaction);
+    }
+}
---
+++ FinanceManager.Infrastructure/External/AiService.cs
@@ -0,0 +1,33 @@
+﻿using FinanceManager.Application.DTOs;
+using FinanceManager.Application.Interfaces;
+using Google.GenAI;
+
+namespace FinanceManager.Infrastructure.External
+{
+    public class AiService : IAiService
+    {
+        public async Task<string> GenerateCategorySuggestionAsync(ReviewTransaction transaction)
+        {
+            var client = new Client(apiKey: "dsilfuaiopueuiwaopuwea");
+
+            var response = await client.Models.GenerateContentAsync(
+                model: "gemini-2.5-flash-lite",
+                contents: [new() { Parts = [new()
+                {
+                    Text = $"Given the following transaction details, suggest a category for it:\n" +
+                            $"Description: {transaction.Description}\n" +
+                            $"Amount: {transaction.Amount}\n" +
+                            $"Date: {transaction.Date:d}"
+                }]}]
+            );
+            var text = response.Candidates?[0].Content?.Parts?[0]?.Text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new Exception("AI model returned empty content.");
+            }
+
+            return text;
+        }
+    }
+}
