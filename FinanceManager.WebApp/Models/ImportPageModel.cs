50c8ffa4f42e651fdf823b171d51f93261811aeb
JoelCunningham <s3949308@student.rmit.edu.au>
2026-02-13 13:12:07 +1100

    FMUS0 -Documentation - Adds copliot instrcutions

--- FinanceManager.WebApp/Models/ImportPageModel.cs
+++ FinanceManager.WebApp/Models/ImportPageModel.cs
@@ -1,99 +1,98 @@
-﻿using FinanceManager.Application.DTOs;
+﻿namespace FinanceManager.WebApp.Models;
+
+using FinanceManager.Application.DTOs;
 using FinanceManager.WebApp.Models.Base;
 
-namespace FinanceManager.WebApp.Models
+public class ImportPageModel
 {
-    public class ImportPageModel
+    public ValidationModel UploadValidation { get; set; } = new();
+    public ValidationModel ImportValidation { get; set; } = new();
+
+    public ParserSummary? SelectedBank { get; set; }
+    public IReadOnlyList<ParsedTransaction>? ImportedTransactions { get; set; }
+    public string SupportedExtensions => GetSupportedExtensions();
+
+    public bool IsPreviewOpen { get; set; } = false;
+    public bool IsSuccessOpen { get; set; } = false;
+
+    public void Reset()
     {
-        public ValidationModel UploadValidation { get; set; } = new();
-        public ValidationModel ImportValidation { get; set; } = new();
+        SelectedBank = null;
+        ImportedTransactions = null;
+        UploadValidation.ClearErrors();
+        IsPreviewOpen = false;
+        IsSuccessOpen = false;
+    }
 
-        public ParserSummary? SelectedBank { get; set; }
-        public IReadOnlyList<ParsedTransaction>? ImportedTransactions { get; set; }
-        public string SupportedExtensions => GetSupportedExtensions();
+    public void UploadSuccess(IEnumerable<ParsedTransaction> records)
+    {
+        ImportedTransactions = [.. records];
+        UploadValidation.SetSucess();
+        IsPreviewOpen = false;
+    }
 
-        public bool IsPreviewOpen { get; set; } = false;
-        public bool IsSuccessOpen { get; set; } = false;
+    public void UploadError(Exception exception)
+    {
+        ImportedTransactions = null;
+        UploadValidation.SetError(GetUploadErrorMessage(exception));
+        IsPreviewOpen = false;
+    }
 
-        public void Reset()
+    public void ImportSuccess()
+    {
+        ImportValidation.SetSucess();
+        IsSuccessOpen = true;
+    }
+
+    public void ImportError()
+    {
+        ImportValidation.SetError("An unexpected error occurred. Please try again.");
+        IsSuccessOpen = false;
+    }
+
+    public void NoTransactionsFound()
+    {
+        ImportedTransactions = null;
+        UploadValidation.SetError("No new transactions were found in the uploaded file.");
+        IsPreviewOpen = false;
+    }
+
+    public void BankChanged(ParserSummary bank)
+    {
+        Reset();
+        SelectedBank = bank;
+    }
+
+    public void ShowPreview()
+    {
+        IsPreviewOpen = true;
+    }
+
+    public void ClosePreview()
+    {
+        IsPreviewOpen = false;
+    }
+
+    public void CloseSuccess()
+    {
+        Reset();
+    }
+
+    private string GetUploadErrorMessage(Exception exception)
+    {
+        if (SelectedBank is null) return "Please select a bank first.";
+
+        return exception switch
         {
-            SelectedBank = null;
-            ImportedTransactions = null;
-            UploadValidation.ClearErrors();
-            IsPreviewOpen = false;
-            IsSuccessOpen = false;
-        }
+            IOException => "The file you uploaded is too large. Please upload a file below 512KB.",
+            KeyNotFoundException => "The type of the file you uploaded is not supported. Please upload a file of type: " + string.Join(", ", SelectedBank.SupportedExtensions),
+            _ => "Unable to import transactions from this file. Please check it is correct."
+        };
+    }
 
-        public void UploadSuccess(IEnumerable<ParsedTransaction> records)
-        {
-            ImportedTransactions = [.. records];
-            UploadValidation.SetSucess();
-            IsPreviewOpen = false;
-        }
-
-        public void UploadError(Exception exception)
-        {
-            ImportedTransactions = null;
-            UploadValidation.SetError(GetUploadErrorMessage(exception));
-            IsPreviewOpen = false;
-        }
-
-        public void ImportSuccess()
-        {
-            ImportValidation.SetSucess();
-            IsSuccessOpen = true;
-        }
-
-        public void ImportError()
-        {
-            ImportValidation.SetError("An unexpected error occurred. Please try again.");
-            IsSuccessOpen = false;
-        }
-
-        public void NoTransactionsFound()
-        {
-            ImportedTransactions = null;
-            UploadValidation.SetError("No new transactions were found in the uploaded file.");
-            IsPreviewOpen = false;
-        }
-
-        public void BankChanged(ParserSummary bank)
-        {
-            Reset();
-            SelectedBank = bank;
-        }
-
-        public void ShowPreview()
-        {
-            IsPreviewOpen = true;
-        }
-
-        public void ClosePreview()
-        {
-            IsPreviewOpen = false;
-        }
-
-        public void CloseSuccess()
-        {
-            Reset();
-        }
-
-        private string GetUploadErrorMessage(Exception exception)
-        {
-            if (SelectedBank is null) return "Please select a bank first.";
-
-            return exception switch
-            {
-                IOException => "The file you uploaded is too large. Please upload a file below 512KB.",
-                KeyNotFoundException => "The type of the file you uploaded is not supported. Please upload a file of type: " + string.Join(", ", SelectedBank.SupportedExtensions),
-                _ => "Unable to import transactions from this file. Please check it is correct."
-            };
-        }
-
-        private string GetSupportedExtensions()
-        {
-            if (SelectedBank is null) return string.Empty;
-            return string.Join(", ", SelectedBank.SupportedExtensions);
-        }
+    private string GetSupportedExtensions()
+    {
+        if (SelectedBank is null) return string.Empty;
+        return string.Join(", ", SelectedBank.SupportedExtensions);
     }
 }
