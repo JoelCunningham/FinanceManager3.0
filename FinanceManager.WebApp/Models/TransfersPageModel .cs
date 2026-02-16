d6e66adf175b94d441672ce40e0950d3276a2f33
JoelCunningham <s3949308@student.rmit.edu.au>
2026-02-17 00:36:30 +1100

    I14 - Implement HAVIT - Updates transfers page

---
+++ FinanceManager.WebApp/Models/TransfersPageModel .cs
@@ -0,0 +1,21 @@
+﻿namespace FinanceManager.WebApp.Models;
+
+using FinanceManager.Application.DTOs;
+using FinanceManager.WebApp.Components.Features.Transfers;
+using Havit.Blazor.Components.Web.Bootstrap;
+
+public class TransfersPageModel
+{
+    public FilterQuery Query { get; set; } = new();
+    public List<string> UniqueAccounts { get; set; } = [];
+    public TransferSummary? SelectedTransfer { get; set; }
+
+    public TransfersGrid? Grid { get; set; }
+    public HxModal DetailsModal { get; set; } = new();
+
+    public async Task OpenDetailsModal(TransferSummary transfer)
+    {
+        SelectedTransfer = transfer;
+        await DetailsModal.ShowAsync();
+    }
+}
